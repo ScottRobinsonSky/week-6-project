b5c44ce30c2293af89373220be3a96c2c89a108b
Scott Robinson <scott.robinson2@sky.uk>
2022-11-10 16:10:19 +0000

    Create database in memory when testing

--- src/db/db.js
+++ src/db/db.js
@@ -5,7 +5,7 @@ const path = require("path");
 //create an instance of the database call it db
 const db = new Sequelize({
     dialect: 'sqlite',
-    storage: path.join(__dirname, '/movie_watchlist.sqlite'),
+    storage: process.env.NODE_ENV === "test" ? ":memory:" : path.join(__dirname, '/movie_watchlist.sqlite'),
     logging: false
 })
 

cce055327fc871883ac1fc542c748460f307e914
Scott Robinson <scott.robinson2@sky.uk>
2022-11-10 16:11:10 +0000

    Improve seeding logic

--- src/db/seed.js
+++ src/db/seed.js
@@ -7,32 +7,30 @@ const fs = require('fs').promises //helps us get access to promises when dealing
 const {db} = require('./db')
 const { Show, User } = require('../models')
 
+async function getJSONFromFile(filepath) {
+    const buffer = await fs.readFile(filepath);
+    return JSON.parse(String(buffer));
+}
+
+async function seedShows() {
+    const showData = await getJSONFromFile(path.join(__dirname, '../data/shows.json'));
+    await Show.bulkCreate(showData);
+}
+
+async function seedUsers() {
+    const userData = await getJSONFromFile(path.join(__dirname, '../data/users.json'));
+    await User.bulkCreate(userData);
+}
 
 //write our seed function -> take our json file, create rows with our data into it
 const seed = async () => {
-
     await db.sync({ force: true }); // clear out database + tables
 
-    const showSeedPath = path.join(__dirname, '../data/shows.json'); //get the path to Show.json file
-    const userSeedPath = path.join(__dirname, '../data/users.json')
-
-
-    const buffer = await fs.readFile(showSeedPath); //asynchronously reads the content in this file
-    const userBuffer = await fs.readFile(userSeedPath);
-
-    const {showsData} = JSON.parse(String(buffer)); // First we convert the data from buffer into a string, then we parse the JSON so it converts from string -> object
-    const {usersData} = JSON.parse(String(userBuffer));
-
-
-    const ShowPromises = showsData.map(show => Show.create(show)); //creates Show and puts it into our Show table
-    const UserPromises = usersData.map(user => User.create(user));
-
-                                        //Show.create({'name': 'Tony', 'age': 25})
-    await Promise.all(ShowPromises); // The Promise.all() method takes an iterable of promises as an input, and returns a single Promise that resolves to an array of the results of the input promises.
-    await Promise.all(UserPromises)
+    await seedShows();
+    await seedUsers();
 
     console.log("Shows and User database info populated!")
 }
 
-//export my seed function
-module.exports = seed;
+// export seed functions
+module.exports = { seed, seedShows, seedUsers };

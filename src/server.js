cd66d5b9a2d2320170b36783919c2ce29893346a
Scott Robinson <scott.robinson2@sky.uk>
2022-11-10 14:27:44 +0000

    Add routes to server.js

--- src/server.js
+++ src/server.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const seed = require("./db/seed");
+const showRouter = require("./routes/shows");
+const userRouter = require("./routes/users");
 
 const app = express();
 app.use(express.json());
+app.use("/shows", showRouter);
+app.use("/users", userRouter);
+
 
 if (require.main === module) {
     app.listen(3000, async () => {

e0f0172cbf111da3da4ecd7e68c091828fd86bfa
Scott Robinson <scott.robinson2@sky.uk>
2022-11-11 11:21:30 +0000

    Add PATCH /shows/:showId endpoint

--- src/routes/shows.js
+++ src/routes/shows.js
@@ -1,4 +1,5 @@
 const { Router } = require("express");
+const { body, validationResult } = require('express-validator');
 const { db } = require("../db/db");
 const { validateShowGenre, validateShowId } = require("../middleware");
 const { Show } = require("../models");
@@ -16,6 +17,44 @@ showRouter.get("/genres", async (_, resp) => {
 showRouter.get("/:showId", validateShowId, async (req, resp) => {
     resp.json(req.show.toJSON());
 });
+
+showRouter.patch(
+    "/:showId",
+    validateShowId,
+    body("rating").custom(value => {
+        if (value === undefined) return true;
+        if (isNaN(+value) || !Number.isInteger(+value) || (value.hasOwnProperty("length") && value.length === 0)) {
+            throw new Error("new rating must be an integer");
+        }
+        if (+value < 0 || +value > 10) throw new Error("new rating must be 0-10 (inclusive)");
+        return true;
+    }),
+    body("status").custom(value => {
+        if (value === undefined) return true;
+        if (typeof(value) !== "string") throw new Error("new status must be a string");
+        if (value.includes(" ")) throw new Error("new status must not contain spaces");
+        if (value.length < 5 || value.length > 25) throw new Error("new status must be 5-25 characters (inclusive)");
+        return true;
+    }),
+    async (req, resp) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            resp.status(400).json(errors.array());
+            return;
+        }
+        const toUpdate = {};
+        if (req.body.rating !== undefined) {
+            toUpdate["rating"] = +req.body.rating;
+        }
+        if (req.body.status !== undefined) {
+            toUpdate["status"] = req.body.status;
+        }
+        if (toUpdate.length !== 0) {
+            await req.show.update(toUpdate);
+        }
+        resp.json(toUpdate);
+    }
+);
 
 showRouter.get("/genres/:genre", validateShowGenre, async (req, resp) => {
     resp.json(await Show.findAll({

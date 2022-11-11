4b48da1ad01446036e586b24a8d3ea8a60b243ee
Scott Robinson <scott.robinson2@sky.uk>
2022-11-11 00:00:29 +0000

    Refactor route tests
    Tests make 1 request per describe block instead of 1 request per test block.

--- tests/routes/shows.test.js
+++ tests/routes/shows.test.js
@@ -13,152 +13,158 @@ describe("Testing /shows endpoint route", () => {
     });
 
     describe("GET /shows/", () => {
+        beforeAll(async () => {
+            resp = await request(app).get("/shows/");
+        });
+
         test("succeeds", async () => {
-            const { statusCode } = await request(app).get("/shows/");
-            expect(statusCode).toBe(200);
+            expect(resp.statusCode).toBe(200);
         });
 
         test("responds with application/json", async () => {
-            const { headers } = await request(app).get("/shows/");
-            expect(headers["content-type"]).toMatch("application/json");
+            expect(resp.headers["content-type"]).toMatch("application/json");
         });
 
         test("responds with all shows", async () => {
-            const { body } = await request(app).get("/shows/");
-            expect(body).toEqual(shows);
+            expect(resp.body).toEqual(shows);
         });
     });
 
     describe("GET /shows/:id", () => {
         describe("with a known id", () => {
+            beforeAll(async () => {
+                resp = await request(app).get(`/shows/${shows[0].id}`);
+            });
+
             test("succeeds", async () => {
-                const { statusCode } = await request(app).get(`/shows/${shows[0].id}`);
-                expect(statusCode).toBe(200);
+                expect(resp.statusCode).toBe(200);
             });
 
             test("responds with application/json", async () => {
-                const { headers } = await request(app).get(`/shows/${shows[0].id}`);
-                expect(headers["content-type"]).toMatch("application/json");
+                expect(resp.headers["content-type"]).toMatch("application/json");
             });
 
             test("responds with the specified show", async () => {
-                const { body } = await request(app).get(`/shows/${shows[0].id}`);
-                expect(body).toEqual(shows[0]);
+                expect(resp.body).toEqual(shows[0]);
             });
         });
 
         describe("with an unknown id", () => {
+            beforeAll(async () => {
+                resp = await request(app).get("/shows/0");
+            });
+
             test("fails with a 404 Not Found", async () => {
-                const { statusCode } = await request(app).get("/shows/0"); // 0 is never a valid id since they start at 1
-                expect(statusCode).toBe(404);
+                expect(resp.statusCode).toBe(404);
             });
 
             test("responds with text/html", async () => {
-                const { headers } = await request(app).get("/shows/0");
-                expect(headers["content-type"]).toMatch("text/html");
+                expect(resp.headers["content-type"]).toMatch("text/html");
             });
 
             test("responds with 'Show Not Found' message", async () => {
-                const { text } = await request(app).get("/shows/0");
-                expect(text).toBe("Show Not Found");
+                expect(resp.text).toBe("Show Not Found");
             });
         });
 
         describe("with an invalid id", () => {
+            beforeAll(async () => {
+                resp = await request(app).get("/shows/foo");
+            });
+
             test("fails with a 400 Bad Request", async () => {
-                const { statusCode } = await request(app).get("/shows/foo");
-                expect(statusCode).toBe(400);
+                expect(resp.statusCode).toBe(400);
             });
 
             test("responds with text/html", async () => {
-                const { headers } = await request(app).get("/shows/foo");
-                expect(headers["content-type"]).toMatch("text/html");
+                expect(resp.headers["content-type"]).toMatch("text/html");
             });
 
             test("responds with 'Show id must be a valid integer' message", async () => {
-                const { text } = await request(app).get("/shows/foo");
-                expect(text).toBe("Show id must be a valid integer.");
+                expect(resp.text).toBe("Show id must be a valid integer.");
             });
         });
     });
 
     describe("GET /shows/genres", () => {
+        beforeAll(async () => {
+            resp = await request(app).get("/shows/genres");
+        });
+
         test("succeeds", async () => {
-            const { statusCode } = await request(app).get("/shows/genres");
-            expect(statusCode).toBe(200);
+            expect(resp.statusCode).toBe(200);
         });
 
         test("responds with application/json", async () => {
-            const { headers } = await request(app).get("/shows/genres");
-            expect(headers["content-type"]).toMatch("application/json");
+            expect(resp.headers["content-type"]).toMatch("application/json");
         });
 
         test("responds with all show genres", async () => {
             const actualGenres = Show.getAttributes().genre.values;
-
-            const { body } = await request(app).get("/shows/genres");
-            expect(body).toEqual(actualGenres);
+            expect(resp.body).toEqual(actualGenres);
         });
     });
 
     describe("GET /shows/genres/:genre", () => {
         describe("with a valid genre", () => {
             const genres = Show.getAttributes().genre.values;
+
             describe("same casing", () => {
-                const genre = genres[0]
+                beforeAll(async () => {
+                    genre = genres[0];
+                    resp = await request(app).get(`/shows/genres/${genre}`);
+                })
+
                 test("succeeds", async () => {
-                    const { statusCode } = await request(app).get(`/shows/genres/${genre}`);
-                    expect(statusCode).toBe(200);
+                    expect(resp.statusCode).toBe(200);
                 });
 
                 test("responds with application/json", async () => {
-                    const { headers } = await request(app).get(`/shows/genres/${genre}`);
-                    expect(headers["content-type"]).toMatch("application/json");
+                    expect(resp.headers["content-type"]).toMatch("application/json");
                 });
 
                 test("responds with all shows matching the genre", async () => {
                     const matchingShows = shows.filter(s => s.genre === genre);
-
-                    const { body } = await request(app).get(`/shows/genres/${genre}`);
-                    expect(body).toEqual(matchingShows.map(s => expect.objectContaining(s)));
+                    expect(resp.body).toEqual(matchingShows.map(s => expect.objectContaining(s)));
                 });
             });
 
             describe("different casing", () => {
-                const genre = genres[0].toUpperCase();
+                beforeAll(async () => {
+                    genre = genres[0].toUpperCase();
+                    resp = await request(app).get(`/shows/genres/${genre}`);
+                });
+
                 test("succeeds", async () => {
-                    const { statusCode } = await request(app).get(`/shows/genres/${genre}`);
-                    expect(statusCode).toBe(200);
+                    expect(resp.statusCode).toBe(200);
                 });
 
                 test("responds with application/json", async () => {
-                    const { headers } = await request(app).get(`/shows/genres/${genre}`);
-                    expect(headers["content-type"]).toMatch("application/json");
+                    expect(resp.headers["content-type"]).toMatch("application/json");
                 });
 
                 test("responds with all shows matching the genre", async () => {
                     const matchingShows = shows.filter(s => s.genre.toLowerCase() === genre.toLowerCase());
-
-                    const { body } = await request(app).get(`/shows/genres/${genre}`);
-                    expect(body).toEqual(matchingShows.map(s => expect.objectContaining(s)));
+                    expect(resp.body).toEqual(matchingShows.map(s => expect.objectContaining(s)));
                 });
             });
         });
 
         describe("with an invalid genre", () => {
+            beforeAll(async () => {
+                resp = await request(app).get("/shows/genres/foo");
+            });
+
             test("fails with a 404 Not Found", async () => {
-                const { statusCode } = await request(app).get("/shows/genres/foo");
-                expect(statusCode).toBe(404);
+                expect(resp.statusCode).toBe(404);
             });
 
             test("responds with text/html", async () => {
-                const { headers } = await request(app).get("/shows/genres/foo");
-                expect(headers["content-type"]).toMatch("text/html");
+                expect(resp.headers["content-type"]).toMatch("text/html");
             });
 
             test("responds with 'Genre Not Found' message", async () => {
-                const { text } = await request(app).get("/shows/genres/foo");
-                expect(text).toBe("Genre Not Found");
+                expect(resp.text).toBe("Genre Not Found");
             });
         });
     });
